2873b30b339e65cfc799bf1bcf988c1095c29fe4
HGallardo-checkpt <109161246+HGallardo-checkpt@users.noreply.github.com>
2023-04-12 17:29:37 -0600

    feat: adding security framework

--- app/src/cpp/native-lib.cpp
+++ app/src/cpp/native-lib.cpp
@@ -1,5 +1,12 @@
 //
 // Created by hgallardo on 11/04/2023.
 //
+#include <jni.h>
+#include <string>
 
-#include "native-lib.h"
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_checkpoint_rfid_1raw_1material_security_jwt_JWTDecoder_getKey(JNIEnv *env, jobject thiz) {
+    std::string b64Key = "Y2Y2MTI3OTVjMTQxYmFhZTczNGJiMjgxZDcwMTM5NTUwMTZlNmYxZjE5MTY2NzkyZGU3YTAzYmQzNGNhOTUyZg==";
+    return env->NewStringUTF(b64Key.c_str());}
--- app/src/main/java/com/checkpoint/rfid_raw_material/security/jwt/JWTDecoder.kt
+++ app/src/main/java/com/checkpoint/rfid_raw_material/security/jwt/JWTDecoder.kt
@@ -1,6 +1,6 @@
 package com.checkpoint.rfid_raw_material.security.jwt
 
-import com.auth0.android.jwt.JWT
+import android.util.Log
 import io.jsonwebtoken.Jwts
 
 
@@ -8,11 +8,19 @@ class JWTDecoder(private val token: String) {
 
 
     fun decode(): String {
+        Log.e("KEY FROM CPP","${getKey()}")
         return Jwts.parserBuilder()
             .setSigningKey("Y2Y2MTI3OTVjMTQxYmFhZTczNGJiMjgxZDcwMTM5NTUwMTZlNmYxZjE5MTY2NzkyZGU3YTAzYmQzNGNhOTUyZg==")
             // TODO: Get from native opcode
             .build()
             .parseClaimsJws(token).body.toString()
     }
+    external fun getKey(): String
 
+    companion object {
+        // Used to load the 'sign-lib' library on application startup.
+        init {
+            System.loadLibrary("sign-lib")
+        }
+    }
 }
